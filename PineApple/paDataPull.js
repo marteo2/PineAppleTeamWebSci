bd16942017f0f284a3ccda5b245e45e785aa91df
Theobromine <isaaczsda@gmail.com>
2018-04-24 19:55:47 -0400

    Fix data pulll

--- PineApple/paDataPull.js
+++ PineApple/paDataPull.js
@@ -19,10 +19,10 @@ var historicalJSON = [{
 	 "bitflyer:btcusd": 11665,
 	 "bitsquare:btcusd": 11383.77,
 	 "bitstamp:btcusd": 11671.25,
-	 "btce:btcusd": 11631.01, 
-	 "cexio:btcusd": 11930.2, 
-	 "gdax:btcusd": 11684.14, 
-	 "gemini:btcusd": 11679.99, 
+	 "btce:btcusd": 11631.01,
+	 "cexio:btcusd": 11930.2,
+	 "gdax:btcusd": 11684.14,
+	 "gemini:btcusd": 11679.99,
 	 "kraken:btcusd": 11688,
 	 "okcoin:btcusd": 12354.36,
 	 "quadriga:btcusd": 11600.01,
@@ -30,15 +30,15 @@ var historicalJSON = [{
 	},{"time": 1519151537.124332,
 		 "bitfinex:btcusd": 11677,
 		 "bitflyer:btcusd": 11665,
-		 "bitsquare:btcusd": 11383.77, 
-		 "bitstamp:btcusd": 11678.7, 
+		 "bitsquare:btcusd": 11383.77,
+		 "bitstamp:btcusd": 11678.7,
 		 "btce:btcusd": 11658,
 		 "cexio:btcusd": 11939.9,
-		 "gdax:btcusd": 11684.14, 
-		 "gemini:btcusd": 11679.17, 
-		 "kraken:btcusd": 11685, 
-		 "okcoin:btcusd": 12354.36, 
-		 "quadriga:btcusd": 11600.01, 
+		 "gdax:btcusd": 11684.14,
+		 "gemini:btcusd": 11679.17,
+		 "kraken:btcusd": 11685,
+		 "okcoin:btcusd": 12354.36,
+		 "quadriga:btcusd": 11600.01,
 		 "quoine:btcusd": 11676},{
   "time": 1519151551.594238,
   "bitfinex:btcusd": 11674,
@@ -56,28 +56,28 @@ var historicalJSON = [{
 },{"time": 1519151912.206002,
 	 "bitfinex:btcusd": 11678,
 	 "bitflyer:btcusd": 11664.9,
-	 "bitsquare:btcusd": 11383.77, 
-	 "bitstamp:btcusd": 11690, 
-	 "btce:btcusd": 11660, 
-	 "cexio:btcusd": 11922.7, 
+	 "bitsquare:btcusd": 11383.77,
+	 "bitstamp:btcusd": 11690,
+	 "btce:btcusd": 11660,
+	 "cexio:btcusd": 11922.7,
 	 "gdax:btcusd": 11675.09,
 	 "gemini:btcusd": 11684.99,
-	 "kraken:btcusd": 11685.1, 
+	 "kraken:btcusd": 11685.1,
 	 "okcoin:btcusd": 12361.22,
-	 "quadriga:btcusd": 11600.01, 
+	 "quadriga:btcusd": 11600.01,
 	 "quoine:btcusd": 11676
 	},{"time": 1519152669.048627,
 		 "bitfinex:btcusd": 11676,
-		 "bitflyer:btcusd": 11706.2, 
+		 "bitflyer:btcusd": 11706.2,
 		 "bitsquare:btcusd": 11383.77,
 		 "bitstamp:btcusd": 11670.89,
-		 "btce:btcusd": 11682.501, 
-		 "cexio:btcusd": 11942.9, 
+		 "btce:btcusd": 11682.501,
+		 "cexio:btcusd": 11942.9,
 		 "gdax:btcusd": 11690,
-		 "gemini:btcusd": 11680.98, 
+		 "gemini:btcusd": 11680.98,
 		 "kraken:btcusd": 11699.4,
-		 "okcoin:btcusd": 12361.22, 
-		 "quadriga:btcusd": 11600.01, 
+		 "okcoin:btcusd": 12361.22,
+		 "quadriga:btcusd": 11600.01,
 		 "quoine:btcusd": 11686.56},{
   "time": 1519152881.957901,
   "bitfinex:btcusd": 11676,
@@ -120,18 +120,18 @@ var historicalJSON = [{
   "okcoin:btcusd": 12453.16,
   "quadriga:btcusd": 11600.01,
   "quoine:btcusd": 11669.852
-},{"time": 1519152944.231408, 
+},{"time": 1519152944.231408,
 	 "bitfinex:btcusd": 11675,
-	 "bitflyer:btcusd": 11676.95, 
+	 "bitflyer:btcusd": 11676.95,
 	 "bitsquare:btcusd": 11383.77,
 	 "bitstamp:btcusd": 11663.79,
 	 "btce:btcusd": 11704.37,
 	 "cexio:btcusd": 11915,
-	 "gdax:btcusd": 11673.8, 
-	 "gemini:btcusd": 11674.3, 
-	 "kraken:btcusd": 11679.8, 
+	 "gdax:btcusd": 11673.8,
+	 "gemini:btcusd": 11674.3,
+	 "kraken:btcusd": 11679.8,
 	 "okcoin:btcusd": 12453.16,
-	 "quadriga:btcusd": 11600.01, 
+	 "quadriga:btcusd": 11600.01,
 	 "quoine:btcusd": 11669.852}
 ];
 
@@ -143,7 +143,7 @@ function aggregateFunction(){
 	var averagePrice = 0;
 	var totalPrice = 0;
 	var marketCount = 0;
-	
+
 	for (var i=0; i<historicalJSON.length; i++){
 		if (historicalJSON[i].time > latestTime){
 			data = historicalJSON[i];
@@ -163,12 +163,12 @@ function aggregateFunction(){
 	marketCount = (dataObjects.length) - 1;
 	averagePrice = totalPrice/marketCount;
 	console.log(averagePrice);
-	
+
 	for (var l= 1; l<dataObjects.length; l++){
 		var difference = (dataObjects[l][1]/averagePrice) - 1.00;
 		var compare = "";
 		var compareID = ""
-		
+
 		if (difference > 0){
 			compare = "above (+" + difference.toFixed(3)+")";
 			compareID = "above";
@@ -190,12 +190,12 @@ function populateChart(form){
 	var times = [];
 	var market1Data = [];
 	var market2Data = [];
-	
+
 	//var utcSeconds = 1234567890;
 	//var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
 	//d.setUTCSeconds(utcSeconds);
 	//d is now a date (in my time zone) set to Fri Feb 13 2009 18:31:30 GMT-0500 (EST)
-	
+
 	for (var i=0; i < historicalJSON.length; i++){
 		var itime = historicalJSON[i].time;
 		var market1point = historicalJSON[i][market1];
@@ -208,7 +208,7 @@ function populateChart(form){
 		market2Data.push(market2point);
 	}
 	console.log(market1Data);
-	
+
 	var ctx = document.getElementById("myChart");
       var myChart = new Chart(ctx, {
         type: 'line',
