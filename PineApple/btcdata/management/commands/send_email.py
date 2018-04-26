5602e8a8899ed21729168379f88af5bb733d15c0
Rowen Qin <chinqrw@gmail.com>
2018-04-25 23:29:30 -0400

    email part fixed

--- PineApple/btcdata/management/commands/send_email.py
+++ PineApple/btcdata/management/commands/send_email.py
@@ -1,8 +1,12 @@
-from django.core.mail import EmailMessage
-from django.template.loader import get_template
+import os
+
+from django.core.mail import EmailMessage, EmailMultiAlternatives
+from django.template.loader import get_template, render_to_string
 from django.utils import timezone
 
 from django.core.management.base import BaseCommand
+from email.mime.image import MIMEImage
+
 from btcdata.getbtcdata import save_btc
 from btcdata.models import Email
 
@@ -14,8 +18,15 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        t = get_template("email/index.html")
-        content = t.render()
-        msg = EmailMessage('User Email', content, 'cmappcmkmarketing@gmail.com', ['chinqrw@gmail.com'])
-        msg.content_subtype = "html"
+        content = render_to_string("email/index.html")
+        msg = EmailMultiAlternatives("User Email", "wow",
+                                     'cmappcmkmarketing@gmail.com', ['chinqrw@gmail.com'])
+        msg.attach_alternative(content, "text/html")
+        msg.mixed_subtype = 'related'
+        for f in ['img-01.png']:
+            fp = open(os.path.join(os.path.dirname(__file__), f), 'rb')
+            msg_img = MIMEImage(fp.read())
+            fp.close()
+            msg_img.add_header('Content-ID', '<{}>'.format(f))
+            msg.attach(msg_img)
         msg.send()
